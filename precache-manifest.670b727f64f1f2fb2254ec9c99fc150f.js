d3e764cf40043761404affce941eca1d2a3716fc
anahitTor <54190917+anahiTor@users.noreply.github.com>
2021-01-20 13:40:22 +0400

    Add files via upload

---
+++ precache-manifest.670b727f64f1f2fb2254ec9c99fc150f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311c77ce6abc8c4efdd0",
+    "url": "/vue-test-live/css/app.a08ed9f5.css"
+  },
+  {
+    "revision": "5734dbedaed7ad49bc43",
+    "url": "/vue-test-live/css/chunk-6605361c.38ce182f.css"
+  },
+  {
+    "revision": "973f4d350edd7608062e33587937567a",
+    "url": "/vue-test-live/db.json"
+  },
+  {
+    "revision": "d7fea33896976a3a78a507f2043e6a7c",
+    "url": "/vue-test-live/index.html"
+  },
+  {
+    "revision": "311c77ce6abc8c4efdd0",
+    "url": "/vue-test-live/js/app.888593c5.js"
+  },
+  {
+    "revision": "5734dbedaed7ad49bc43",
+    "url": "/vue-test-live/js/chunk-6605361c.f7330a9a.js"
+  },
+  {
+    "revision": "f460f25ed658b2d77dea",
+    "url": "/vue-test-live/js/chunk-vendors.cd0d1216.js"
+  },
+  {
+    "revision": "c6c67667772a7fd8c1cc815b262b570d",
+    "url": "/vue-test-live/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test-live/robots.txt"
+  }
+]);
